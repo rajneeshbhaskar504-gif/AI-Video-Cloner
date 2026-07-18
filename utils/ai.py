14dd3f51a7291378c1d8152988b2308f1d6a0b19
rajneeshbhaskar504-gif <rajneeshbhaskar504@gmail.com>
2026-07-18 20:56:40 +0530

    Update ai.py

--- utils/ai.py
+++ utils/ai.py
@@ -1,1 +1,16 @@
+import os
+import google.generativeai as genai
 
+class AIEngine:
+    def __init__(self):
+        api_key = os.getenv("GEMINI_API_KEY")
+
+        if not api_key:
+            raise ValueError("GEMINI_API_KEY not found in .env file")
+
+        genai.configure(api_key=api_key)
+        self.model = genai.GenerativeModel("gemini-1.5-flash")
+
+    def generate(self, prompt):
+        response = self.model.generate_content(prompt)
+        return response.text
