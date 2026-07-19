b017989c3faad00b69841a1435cfa49cec20d02f
rajneeshbhaskar504-gif <rajneeshbhaskar504@gmail.com>
2026-07-19 12:58:55 +0530

    Update ai.py

--- utils/ai.py
+++ utils/ai.py
@@ -1,16 +1,92 @@
-import os
+# u
+
 import google.generativeai as genai
+from config import GEMINI_API_KEY
 
 class AIEngine:
+
     def __init__(self):
-        api_key = os.getenv("GEMINI_API_KEY")
 
-        if not api_key:
-            raise ValueError("GEMINI_API_KEY not found in .env file")
+        if not GEMINI_API_KEY:
+            raise Exception(
+                "Gemini API Key not found."
+            )
 
-        genai.configure(api_key=api_key)
-        self.model = genai.GenerativeModel("gemini-1.5-flash")
+        genai.configure(
+            api_key=GEMINI_API_KEY
+        )
 
-    def generate(self, prompt):
-        response = self.model.generate_content(prompt)
+        self.model = genai.GenerativeModel(
+            "gemini-2.5-flash"
+        )
+
+    def generate_script(
+        self,
+        topic,
+        language="Hindi",
+        duration=5
+    ):
+
+        prompt = f"""
+
+Create an original YouTube video script.
+
+Topic:
+{topic}
+
+Language:
+{language}
+
+Duration:
+{duration} Minutes
+
+Rules:
+
+• Original Script
+• Engaging
+• Professional
+• Narration Only
+• No Copyright Content
+
+"""
+
+        response = self.model.generate_content(
+            prompt
+        )
+
         return response.text
+
+    def generate_scene_prompts(
+        self,
+        script
+    ):
+
+        prompt = f"""
+
+Read this script.
+
+Create one cinematic AI image prompt for every scene.
+
+Only return prompts.
+
+Script:
+
+{script}
+
+"""
+
+        response = self.model.generate_content(
+            prompt
+        )
+
+        prompts = []
+
+        for line in response.text.split("\n"):
+
+            line = line.strip()
+
+            if line:
+
+                prompts.append(line)
+
+        return prompts
